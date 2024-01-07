269da9df765ade965e86fa8f4745ba9c607e2148
Fernando JS Silva <ferjssilva@gmail.com>
2024-01-07 01:02:24 -0300

    feat: Update App layout on main page

--- src/page-containers/AppRoot/App.js
+++ src/page-containers/AppRoot/App.js
@@ -11,6 +11,11 @@ const habitsMock = [
 
 function App() {
   const [habits, setHabits] = useState(habitsMock);
+  const [answer, setAnswer] = useState('Yes');
+
+  const handleChange = (event) => {
+    setAnswer(event.target.value);
+  }
 
   function habitStatusToClass(status) {
     switch (status) {
@@ -24,20 +29,115 @@ function App() {
 
   return (
     <div className="App">
-      {habits.map((habit, index) => (
-        <div key={index}>
-          <span className={habitStatusToClass(habit.status)}>
-            {habit.name}
-          </span>
-          <button
-            type='button'
-            className='nes-btn'
-            onClick={() => { /* removeTask(habit.id) */ }}
-          >Remove</button>
+      <div class="nes-container with-title is-centered" style={{ textAlign: 'left' }}>
+        <p class="title">Fer - Lvl 2</p>
+        <div>
+          <span class="nes-text">Health - </span>
+          <i class="nes-icon is-small heart"></i>
+          <i class="nes-icon is-small heart is-half"></i>
+          <i class="nes-icon is-small heart is-transparent"></i>
+          <i class="nes-icon is-small heart is-empty"></i>
+          <i class="nes-icon is-small heart is-empty"></i>
         </div>
-      ))}
-      <button onClick={() => console.log("cliked") }>Add Task</button>
-      <button type='button' className='nes-btn is-disabled'>Remove</button>
+        <div>
+          <span class="nes-text">Win Streak - 2x </span>
+          <i class="nes-icon trophy is-small"></i>
+          <br />
+          <br />
+
+          <progress className="nes-progress is-primary" value="75" max="100"></progress>
+        </div>
+      </div>
+      <br />
+
+      <div class="nes-container with-title is-centered">
+        <p class="title">Hoje - Domingo</p>
+        <div class="nes-field" style={{ display: 'flex' }}>
+          <input id="time_field" class="nes-input" type="date" style={{ marginRight: '8px' }} />
+          <button type="button" class="nes-btn is-primary">Add</button>
+        </div>
+      </div>
+      <br />
+      <div class="nes-container with-title is-centered">
+      <p class="title">Hábitos</p>
+        {habits.map((habit, index) => (
+          <div key={index}>
+            <label>
+              <input type="checkbox" class="nes-checkbox" checked />
+              <span className={habitStatusToClass(habit.status)}>
+                <del>{habit.name}</del>
+              </span>
+            </label>
+          </div>
+        ))}
+      </div>
+      <br />
+      <div class="nes-container with-title is-centered">
+        <p class="title">Manhã</p>
+        {habits.map((habit, index) => (
+          <div key={index}>
+            <label>
+              <input type="checkbox" class="nes-checkbox" checked />
+              <span className={habitStatusToClass(habit.status)}>
+                <del>{habit.name}</del>
+              </span>
+            </label>
+          </div>
+        ))}
+      </div>
+      <br />
+      <div class="nes-container with-title is-centered">
+        <p class="title">Tarde</p>
+        {habits.map((habit, index) => (
+          <div key={index}>
+            <label>
+              <input type="checkbox" class="nes-checkbox" checked />
+              <span className={habitStatusToClass(habit.status)}>
+                <del>{habit.name}</del>
+              </span>
+            </label>
+          </div>
+        ))}
+      </div>
+      <br />
+      <div class="nes-container with-title is-centered">
+        <p class="title">Noite</p>
+        {habits.map((habit, index) => (
+          <div key={index}>
+            <label>
+              <input type="checkbox" class="nes-checkbox" checked />
+              <span className={habitStatusToClass(habit.status)}>
+                <del>{habit.name}</del>
+              </span>
+            </label>
+          </div>
+        ))}
+      </div>
+      <br />
+      <div style={{ position: 'fixed', bottom: 0, width: '100%', backgroundColor: 'white', padding: '10px' }}>
+        <div class="nes-container is-rounded">
+          <div>
+            <label>
+              <input type="radio" class="nes-radio" name="answer" value="Yes" checked={answer === 'Yes'} onChange={handleChange} />
+              <span>Habits</span>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              <input type="radio" class="nes-radio" name="answer" value="No" checked={answer === 'No'} onChange={handleChange} />
+              <span>History</span>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              <input type="radio" class="nes-radio" name="answer" value="Maybe" checked={answer === 'Maybe'} onChange={handleChange} />
+              <span>Config</span>
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
