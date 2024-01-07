7662fc980af0b65002cf58b4386cb1177eb4a0cf
Fernando JS Silva <ferjssilva@gmail.com>
2024-01-06 23:15:44 -0300

    maint: replace style location from index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './page-containers/AppRoot/App';
 import reportWebVitals from './utils/reportWebVitals';
 
