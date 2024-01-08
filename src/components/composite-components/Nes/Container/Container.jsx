b8d8cd4b49aa43db1c97eb97de005b1b74eb055e
Fernando JS Silva <ferjssilva@gmail.com>
2024-01-08 18:36:18 -0300

    docs: add JSDoc comments to Container component

--- src/components/composite-components/Nes/Container/Container.jsx
+++ src/components/composite-components/Nes/Container/Container.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+/**
+ * Container component
+ * It is considered a composite component because it is composed of other components
+ * and it has a purpose of its own.
+ * @param {boolean} centered - Center the container
+ * @param {boolean} dark - Darken the container
+ * @param {boolean} rounded - Round the container
+ * @param {string} title - Title of the container
+ * @param {node} children - Child elements
+ * @return {jsx}
+ */
+
 function Container({ centered, dark, rounded, title, children }) {
   const classes = classNames({
     'nes-container': true,
