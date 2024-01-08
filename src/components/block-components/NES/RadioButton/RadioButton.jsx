eb1cd3fd14345544e1d4bbe165169b34649dcb08
Fernando JS Silva <ferjssilva@gmail.com>
2024-01-08 18:52:52 -0300

    feat: add documentation for RadioButton component

--- src/components/block-components/NES/RadioButton/RadioButton.jsx
+++ src/components/block-components/NES/RadioButton/RadioButton.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+/**
+ * RadioButton component
+ * It is considered a block component because it can be used independently and has its own styling.
+ * @param {string} label - Label of the radio button
+ * @param {boolean} checked - Whether the radio button is checked
+ * @param {string} group - Name of the group the radio button belongs to
+ * @param {boolean} dark - Darken the radio button
+ * @param {function} onChange - Function to call when the radio button is clicked
+ * @param {node} children - Child elements
+ * @return {jsx}
+ */
+
 const RadioButton = ({ label, checked, group, dark, onChange, children }) => {
   const classes = classNames({
     'nes-radio': true,
