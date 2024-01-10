f0217251461bc7d29c843c3df9f0cd6d1ea21ea9
Fernando JS Silva <ferjssilva@gmail.com>
2024-01-10 03:37:29 -0300

    feat: add table and navigation buttons to History page

--- src/page-containers/History/History.jsx
+++ src/page-containers/History/History.jsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import Container from '../../components/block-components/NES/Container/Container';
 
 const Home = () => (
   <div>
-    <h1>Welcome to the History Page!</h1>
-    <p>This is the content of the history page.</p>
+    <Container
+      title={'October - Week 1/4'}
+      centered
+    >
+      <div className="nes-table-responsive">
+        <table className="nes-table is-centered s">
+          <thead>
+            <tr>
+              <th>Habit</th>
+              <th>Sun</th>
+              <th>Mon</th>
+              <th>Tue</th>
+              <th>Wed</th>
+              <th>Thd</th>
+              <th>Fri</th>
+              <th>Sat</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Drinking Water</td>
+              <td>✅</td>
+              <td>✅</td>
+              <td>✅</td>
+              <td>❌</td>
+              <td>❌</td>
+              <td>✅</td>
+              <td>❌</td>
+            </tr>
+            <tr>
+              <td>Dancing</td>
+              <td>✅</td>
+              <td>✅</td>
+              <td>✅</td>
+              <td>❌</td>
+              <td>❌</td>
+              <td>✅</td>
+              <td>❌</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Container>
+    <br />
+    <Container>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <button
+          className="nes-btn is-primary"
+          style={{ width: '45%' }}
+        >
+          {'<<'}
+        </button>
+        <button
+          className="nes-btn is-primary"
+          style={{ width: '45%' }}
+        >
+          {'>>'}
+        </button>
+      </div>
+    </Container>
   </div>
 );
 
