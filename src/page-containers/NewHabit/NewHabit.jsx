be9b6118bce78c8c75be675710172a58530ab972
Fernando JS Silva <ferjssilva@gmail.com>
2024-01-10 02:15:24 -0300

    feat: add NewHabit page

---
+++ src/page-containers/NewHabit/NewHabit.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NewHabit = () => (
+  <div>
+    <h1>Welcome to the New Habits Page!</h1>
+    <p>This is the content of the Habits page.</p>
+  </div>
+);
+
+export default NewHabit;
