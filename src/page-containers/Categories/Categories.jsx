cf3565975bea884cf7cd21690b386a1f6d25a9d9
Fernando JS Silva <ferjssilva@gmail.com>
2024-01-10 04:16:01 -0300

    feat: add category buttons with icons and descriptions

--- src/page-containers/Categories/Categories.jsx
+++ src/page-containers/Categories/Categories.jsx
@@ -1,10 +1,89 @@
 import React from 'react';
+import Container from '../../components/block-components/NES/Container/Container';
 
-const Home = () => (
-  <div>
-    <h1>Categories</h1>
-    <p>Wohooo</p>
-  </div>
-);
+import { MdSportsSoccer } from 'react-icons/md';
+import { TbBulb } from 'react-icons/tb';
+import { BsFillSuitHeartFill, BsYinYang } from 'react-icons/bs';
+import { FaUsers } from 'react-icons/fa';
+
+function Home() {
+  return (
+    <Container
+      title={'Categories'}
+      centered
+    >
+      <div>
+        <label
+          className="nes-btn"
+          style={{ width: '100%' }}
+        >
+          <h2>
+            <MdSportsSoccer size={48} /> Physical
+          </h2>
+          <p>
+            Take care of your body to maintain health now and in the future.
+          </p>
+        </label>
+      </div>
+      <br />
+      <div>
+        <label
+          className="nes-btn"
+          style={{ width: '100%' }}
+        >
+          <h2>
+            <TbBulb size={48} /> Intellectual
+          </h2>
+          <p>Develop your brain by valuing mental challenges and curiosity.</p>
+        </label>
+      </div>
+      <br />
+      <div>
+        <label
+          className="nes-btn"
+          style={{ width: '100%' }}
+        >
+          <h2>
+            <BsFillSuitHeartFill size={48} /> Emotional
+          </h2>
+          <p>
+            Recognize and respect your feelings, seeking balance. Cultivate
+            empathy and self-awareness.
+          </p>
+        </label>
+      </div>
+      <br />
+      <div>
+        <label
+          className="nes-btn"
+          style={{ width: '100%' }}
+        >
+          <h2>
+            <FaUsers size={48} /> Social
+          </h2>
+          <p>
+            Foster healthy relationships and a strong connection with your
+            community.
+          </p>
+        </label>
+      </div>
+      <br />
+      <div>
+        <label
+          className="nes-btn"
+          style={{ width: '100%' }}
+        >
+          <h2>
+            <BsYinYang size={48} /> Spiritual
+          </h2>
+          <p>
+            Find a sense of purpose and meaning in existence. Seek inner peace,
+            ethical and moral values.
+          </p>
+        </label>
+      </div>
+    </Container>
+  );
+}
 
 export default Home;
