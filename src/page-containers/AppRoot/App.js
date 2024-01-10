bcc06b2ca14453e7c805e9b7c8f62c8450b22214
Fernando JS Silva <ferjssilva@gmail.com>
2024-01-10 01:28:42 -0300

    feat: add NavBoard component to App.js

--- src/page-containers/AppRoot/App.js
+++ src/page-containers/AppRoot/App.js
@@ -7,27 +7,41 @@ import Categories from '../Categories/Categories';
 import History from '../History/History';
 import Settings from '../Settings/Settings';
 
+import NavBoard from '../../components/composite-components/NavBoard/NavBoard';
+
 /* ------------------------------- CSS Imports ------------------------------ */
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Route
-        path="/"
-        component={Home}
-      />
+      <Route path="/">
+        {() => (
+          <div>
+            <Home />
+            <NavBoard checked="habits" />
+          </div>
+        )}
+      </Route>
+      <Route path="/history">
+        {() => (
+          <div>
+            <History />
+            <NavBoard checked="history" />
+          </div>
+        )}
+      </Route>
+      <Route path="/settings">
+        {() => (
+          <div>
+            <Settings />
+            <NavBoard checked="settings" />
+          </div>
+        )}
+      </Route>
       <Route
         path="/categories"
         component={Categories}
-      />
-      <Route
-        path="/history"
-        component={History}
-      />
-      <Route
-        path="/settings"
-        component={Settings}
       />
     </div>
   );
