33998a2ad18c2fa5929ca7c44a4100ad26c7aa17
Fernando JS Silva <ferjssilva@gmail.com>
2024-01-09 22:40:28 -0300

    feat: add Dashboard component

---
+++ src/components/domain-components/Dashboard/Dashboard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Container from '../../components/block-components/NES/Container/Container';
+const Home = () => (
+  <Container
+    title={'Fer - Lvl 2'}
+    centered
+  >
+    <div>
+      <span className="nes-text">Health - </span>
+      <i className="nes-icon is-small heart"></i>
+      <i className="nes-icon is-small heart is-half"></i>
+      <i className="nes-icon is-small heart is-transparent"></i>
+      <i className="nes-icon is-small heart is-empty"></i>
+      <i className="nes-icon is-small heart is-empty"></i>
+    </div>
+    <div>
+      <span className="nes-text">Win Streak - 2x </span>
+      <i className="nes-icon trophy is-small"></i>
+
+      <br />
+      <br />
+
+      <progress
+        className="nes-progress is-primary"
+        value="75"
+        max="100"
+      />
+    </div>
+  </Container>
+);
+
+export default Home;
