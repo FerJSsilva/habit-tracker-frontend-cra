427ddbd0dee0185f40aae25d625ebdf5a61f9ca7
Fernando JS Silva <ferjssilva@gmail.com>
2024-02-17 18:50:31 -0300

    tests: Add tests for App component

--- src/page-containers/AppRoot/App.test.js
+++ src/page-containers/AppRoot/App.test.js
@@ -1,8 +1,65 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('../Home/Home', () => {
+  const Home = () => <div>Home Component</div>;
+  Home.displayName = 'Home';
+  return Home;
+});
+
+jest.mock('../History/History', () => {
+  const History = () => <div>History Component</div>;
+  History.displayName = 'History';
+  return History;
+});
+
+jest.mock('../Settings/Settings', () => {
+  const Settings = () => <div>Settings Component</div>;
+  Settings.displayName = 'Settings';
+  return Settings;
+});
+
+jest.mock('../Error/Error', () => {
+  const Error = () => <div>Error Component</div>;
+  Error.displayName = 'ErrorPage';
+  return Error;
+});
+
+jest.mock('../../components/composite-components/NavBoard/NavBoard', () => {
+  const NavBoard = () => <div>NavBoard Component</div>;
+  NavBoard.displayName = 'NavBoard';
+  return NavBoard;
+});
+
+describe('App Component', () => {
+  test('The `App` function should render the `Home` and `NavBoard` components when the path is "/"', () => {
+    window.history.pushState({}, '', '/');
+    render(<App />);
+    expect(screen.getByText('Home Component')).toBeInTheDocument();
+    expect(screen.getByText('NavBoard Component')).toBeInTheDocument();
+  });
+
+  test('The `App` function should render the `History` and `NavBoard` components when the path is "/history"', () => {
+    window.history.pushState({}, '', '/history');
+    render(<App />);
+    expect(screen.getByText('History Component')).toBeInTheDocument();
+    expect(screen.getByText('NavBoard Component')).toBeInTheDocument();
+  });
+
+  test('The `App` function should render the `Settings` and `NavBoard` components when the path is "/settings"', () => {
+    window.history.pushState({}, '', '/settings');
+    render(<App />);
+    expect(screen.getByText('Settings Component')).toBeInTheDocument();
+    expect(screen.getByText('NavBoard Component')).toBeInTheDocument();
+  });
+
+  test('The `App` function should render the `Home` and `NavBoard` components when the path is "/"', () => {
+    window.history.pushState({}, '', '/');
+    render(<App />);
+    expect(screen.getByText('Home Component')).toBeInTheDocument();
+    expect(screen.getByText('NavBoard Component')).toBeInTheDocument();
+  });
 });
